4e149058b4c5451315dd2799d4fdbd642873aee9
metayerromain <romain.metayer@hetic.net>
2018-10-29 15:20:54 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import './App.css';
+import HomeContainer from '../../views/Home/homeContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      hello
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/Home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => { 
+    return(
+        <section>
+            <h1>
+                Homepage
+            </h1>
+        </section>
+    );
+}
+
+export default Home;
