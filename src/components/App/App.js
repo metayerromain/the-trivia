63b8e26a3f3b76b85541fd564f8cdeb670dcaf96
metayerromain <romain.metayer@hetic.net>
2019-01-13 18:32:16 +0100

    bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import HomeContainer from '../../views/Home/HomeContainer';
+import HomeContainer from '../../views/Home/homeContainer';
 import CategoryContainer from '../../views/Category/CategoryContainer';
 
 class App extends Component {
